c2345a258231ae5cbab56763095df4fd7f8361f3
Philip O'Toole <philip.otoole@yahoo.com>
2025-05-09 08:46:53 -0400

    Correct URL parameters name

--- src/main/java/com/example/RqliteClient.java
+++ src/main/java/com/example/RqliteClient.java
@@ -709,7 +709,7 @@ class NodeOptions {
     public Duration timeout;
     @UValue(value = "pretty", omitEmpty = true)
     public boolean pretty;
-    @UValue(value = "non_voters", omitEmpty = true)
+    @UValue(value = "nonvoters", omitEmpty = true)
     public boolean nonVoters;
     @UValue(value = "ver", omitEmpty = true)
     public String version;
